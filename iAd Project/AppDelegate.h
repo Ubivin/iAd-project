75a7008c1058f468e66613930f95fbd5a2d3384d
Ubivin <ubivin@gmail.com>
2014-10-23 17:08:26 -0700

    Initial Commit

---
+++ iAd Project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iAd Project
+//
+//  Created by Jason Hoang on 10/23/14.
+//  Copyright (c) 2014 Jason Hoang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iAd Project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iAd Project
+//
+//  Created by Jason Hoang on 10/23/14.
+//  Copyright (c) 2014 Jason Hoang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
