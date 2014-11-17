a8f4e512193e06ec70b9f1eb110a08e9b42a8c2d
Ubivin <ubivin@gmail.com>
2014-11-16 22:55:00 -0800

    update ad

--- iAd Project/ViewController.m
+++ iAd Project/ViewController.m
@@ -6,11 +6,17 @@
 //  Copyright (c) 2014 Jason Hoang. All rights reserved.
 //
 
+//still not fully functional, still needs to be reimplemented
+
 #import "ViewController.h"
-#import <iAd/iAd.h>
+
 
 @interface ViewController ()
-
+{
+    
+    BOOL _bannerIsVisible;
+    ADBannerView *_adBanner;
+}
 @end
 
 @implementation ViewController
@@ -29,8 +35,24 @@
     
     [super viewDidAppear:animated];
     
-    ADBannerView *adView = [[ADBannerView alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height - 50,320,50)];
-    [self.view addSubview:adView];
+    _adBanner = [[ADBannerView alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height - 50,320,50)];
+    _adBanner.delegate = self;
+    
+}
+-(void)bannerViewDidLoadAd:(ADBannerView *)banner{
+    
+    if (!_bannerIsVisible)
+    {
+        //if banner isn't part of view hierarchy add it
+        if(_adBanner.superview == nil)
+        {
+            [self.view addSubview:_adBanner];
+            
+        }
+        UIView
+    }
+    
+    
 }
 
 @end
